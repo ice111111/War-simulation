cae63e36202ef14f2bdd9aef7a7fcea996c67a2e
ice111111 <96037997+ice111111@users.noreply.github.com>
2021-12-12 21:30:48 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    //Create a new war game
+    Deck d = new Deck();
+    
+    
+    //Call playWar()
+    War w = new War();
+    w.playWar();
+  }
+}
